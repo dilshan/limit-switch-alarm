0f4ec2299e1f2c73a17a54862c2ff243b37981ea
dilshan <jayakody2000lk@gmail.com>
2024-10-11 07:29:44 +0530

    Minor source code refactoring.

--- firmware/main.c
+++ firmware/main.c
@@ -6,6 +6,28 @@
  * https://github.com/dilshan/limit-switch-alarm
  *
  * Created on October 6, 2024, 12:46 PM
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2024 Dilshan R Jayakody
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #include "fuseconfig.h"
@@ -15,6 +37,15 @@
 #define ALARM_MUTE      0x02
 #define BUZZER_PLUSE    0x04
 #define BUZZER_CONTINUE 0x08
+
+#define BASE_TIMER_LIMIT  900   // Closer to 10 second (adjusted by considering the code execution delays).
+#define MUTE_TIMEOUT_LIMIT  30  // Closer to 5 minutes. (MUTE_TIMEOUT_LIMIT * 10 * BASE_TIMER_LIMIT)/(60 * 1000)
+
+#define RING_CONTINUE_LIMIT 20
+
+// Buzzer on and off delay limits.
+#define RING_OFF_LIMIT  750
+#define RING_ON_LIMIT   250
 
 void main(void)
 {
@@ -80,14 +111,14 @@ void main(void)
       // Timeout counter.
       if(controllerState & ALARM_MUTE)
       {
-        if((++baseTimer) >= 900)
+        if((++baseTimer) >= BASE_TIMER_LIMIT)
         {
-          // 10sec delay has been reached!
+          // base time delay has been reached (10 seconds in default configuration)!
           baseTimer = 0; 
           
-          if((++timeout) >= 30)
+          if((++timeout) >= MUTE_TIMEOUT_LIMIT)
           {
-            //5min timeout delay has been reached!
+            // mute timeout delay has been reached!
             timeout = 0;
             
             controllerState = controllerState & (~ALARM_MUTE);
@@ -100,26 +131,26 @@ void main(void)
                                            && ((controllerState & BUZZER_CONTINUE) == 0x00))
       {
         // Alarm is running and to avoid buzzer heat-ups, lets pulse the output.
-        if(((controllerState & BUZZER_PLUSE) == 0x00) && ((++pulseTimer) >= 750))
+        if(((controllerState & BUZZER_PLUSE) == 0x00) && ((++pulseTimer) >= RING_OFF_LIMIT))
         {
-          // Turn off buzzer after 15sec ring!
+          // Turn off buzzer (15sec in default configuration)!
           GPIO = GPIO & 0xEF;   
           pulseTimer = 0;
           controllerState = controllerState | BUZZER_PLUSE;
         }
         
-        if((controllerState & BUZZER_PLUSE) && ((++pulseTimer) >= 250))
+        if((controllerState & BUZZER_PLUSE) && ((++pulseTimer) >= RING_ON_LIMIT))
         {
           
-          // Turn on buzzer after 5sec delay!
+          // Turn on buzzer (5sec in default configuration)!
           GPIO = GPIO | 0x10;
           pulseTimer = 0;
           controllerState = controllerState & (~BUZZER_PLUSE);
           buzzerCount++;
         } 
 
-        // After 20 beep cycles, buzzer starts to ring continuously...
-        if(buzzerCount > 20)
+        // After defined beep cycles, buzzer starts to ring continuously...
+        if(buzzerCount > RING_CONTINUE_LIMIT)
         {
           GPIO = GPIO | 0x10;
           controllerState = controllerState | BUZZER_CONTINUE;
